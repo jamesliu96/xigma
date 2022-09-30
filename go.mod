b8ef92178fb58832043e025fa31b17d883bdad02
James Liu <2211002+jamesliu96@users.noreply.github.com>
2022-09-30 10:37:00 +0000

    build: go1.19.1

--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/jamesliu96/xigma
 go 1.19
 
 require (
-	github.com/jamesliu96/geheim v1.20.0
-	github.com/jamesliu96/xp v1.3.1
-	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035
+	github.com/jamesliu96/geheim v1.20.1
+	github.com/jamesliu96/xp v1.3.2
+	golang.org/x/term v0.0.0-20220919170432-7a66f970e087
 )
 
 require (
-	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
-	golang.org/x/sys v0.0.0-20220808155132-1c4a2a72c664 // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
 )
--- xm.sh
+++ xm.sh
@@ -14,55 +14,16 @@ if [[ $1 = "-build" ]]; then
   printf "removing \"$outdir\" ... "
   rm -rf $outdir && echo "SUCCESS" || echo "FAILED"
   ldflags="$ldflags -s -w"
-  osarchs=(
-    # "aix ppc64"
-    # "android amd64"
-    # "android arm"
-    # "android arm64"
-    "darwin amd64"
-    "darwin arm64"
-    # "dragonfly amd64"
-    # "freebsd 386"
-    # "freebsd amd64"
-    # "freebsd arm"
-    # "illumos amd64"
-    # "ios arm64"
-    # "js wasm"
-    "linux 386"
-    "linux amd64"
-    "linux arm"
-    "linux arm64"
-    "linux ppc64"
-    "linux ppc64le"
-    "linux mips"
-    "linux mipsle"
-    "linux mips64"
-    "linux mips64le"
-    "linux riscv64"
-    "linux s390x"
-    "linux loong64"
-    # "netbsd 386"
-    # "netbsd amd64"
-    # "netbsd arm"
-    # "openbsd 386"
-    # "openbsd amd64"
-    # "openbsd arm"
-    # "openbsd arm64"
-    # "plan9 386"
-    # "plan9 amd64"
-    # "plan9 arm"
-    # "solaris amd64"
-    "windows 386"
-    "windows amd64"
-    "windows arm"
-    "windows arm64"
-  )
+  osarchs=$(go tool dist list)
   set +e
-  for i in "${osarchs[@]}"; do
+  for i in $osarchs; do
+    IFS="/"
     osarch=($i)
+    unset IFS
     os=${osarch[0]}
     arch=${osarch[1]}
     suffix=
+    [[ $os = "android" || $os = "ios" || $os = "js" ]] && continue
     [[ $os = "windows" ]] && suffix=".exe"
     [[ $arch = "wasm" ]] && suffix=".wasm"
     out="${outdir}/${app}_${os}_$arch$suffix"
