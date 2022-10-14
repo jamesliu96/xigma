3aad95f65477b4b8101d86aca35b2ee4cf75f8d9
James Liu <2211002+jamesliu96@users.noreply.github.com>
2022-10-14 08:38:55 +0000

    build: upgrade deps

--- cmd/xm/xm.go
+++ cmd/xm/xm.go
@@ -12,8 +12,8 @@ import (
 	"os"
 	"sort"
 
+	"github.com/jamesliu96/geheim"
 	"github.com/jamesliu96/xigma"
-	"github.com/jamesliu96/xp"
 )
 
 const app = "xm"
@@ -89,13 +89,13 @@ func main() {
 				}
 			}
 			log.Println("resolved", hex.EncodeToString(clientPub))
-			serverPriv, serverPub, err := xp.P()
+			serverPriv, serverPub, err := geheim.P()
 			if err != nil {
 				rw.WriteHeader(http.StatusInternalServerError)
 				return
 			}
 			rw.Header().Set(HEADER_SERVER, hex.EncodeToString(serverPub))
-			shared, err := xp.X(serverPriv, clientPub)
+			shared, err := geheim.X(serverPriv, clientPub)
 			if err != nil {
 				rw.WriteHeader(http.StatusBadRequest)
 				return
@@ -170,14 +170,14 @@ func main() {
 		}
 		var clientPriv, clientPub []byte
 		if key != nil {
-			if pub, err := xp.X(key, nil); err != nil {
+			if pub, err := geheim.X(key, nil); err != nil {
 				log.Fatalln(err)
 				return
 			} else {
 				clientPriv, clientPub = key, pub
 			}
 		} else {
-			if priv, pub, err := xp.P(); err != nil {
+			if priv, pub, err := geheim.P(); err != nil {
 				log.Fatalln(err)
 				return
 			} else {
@@ -197,7 +197,7 @@ func main() {
 				log.Fatalln(err)
 				return
 			}
-			shared, err := xp.X(clientPriv, serverPub)
+			shared, err := geheim.X(clientPriv, serverPub)
 			if err != nil {
 				log.Fatalln(err)
 				return
--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/jamesliu96/xigma
 go 1.19
 
 require (
-	github.com/jamesliu96/geheim v1.20.1
-	github.com/jamesliu96/xp v1.3.2
+	github.com/jamesliu96/geheim v1.20.4
 	golang.org/x/term v0.0.0-20220919170432-7a66f970e087
 )
 
 require (
-	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
-	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/crypto v0.0.0-20221012134737-56aed061732a // indirect
+	golang.org/x/sys v0.0.0-20221013171732-95e765b1cc43 // indirect
 )
